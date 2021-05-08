604bff9cac0c8f3196362df752cec8d8eca812b7
Boinkie <spamrentier@gmail.com>
2021-05-08 16:53:12 +0200

    Oefeningen Week 1

--- Oefening_1-5.php
+++ Oefening_1-5.php
@@ -12,22 +12,6 @@ foreach ($students as $student) {
     }   
 }
 
-function array_orderby()
-{
-    $args = func_get_args();
-    $data = array_shift($args);
-    foreach ($args as $n => $field) {
-        if (is_string($field)) {
-            $tmp = array();
-            foreach ($data as $key => $row)
-                $tmp[$key] = $row[$field];
-            $args[$n] = $tmp;
-            }
-    }
-    $args[] = &$data;
-    call_user_func_array('array_multisort', $args);
-    return array_pop($args);
-}
 
 ?>
 <!DOCTYPE html>
